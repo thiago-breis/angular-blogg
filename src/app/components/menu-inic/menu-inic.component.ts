8f21578c39cb7477731527a559cb1e319fe3012d
Thiago Barreto Reis <aprendizthiago@hotmail.com>
2023-10-11 18:27:13 -0300

    content creat

---
+++ src/app/components/menu-inic/menu-inic.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-inic',
+  templateUrl: './menu-inic.component.html',
+  styleUrls: ['./menu-inic.component.css']
+})
+export class MenuInicComponent {
+
+}
